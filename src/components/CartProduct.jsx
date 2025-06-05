1581cf10c7423f4fdee969f6e8578bfb286e00ac
Danin <ninsweetie859@gmail.com>
2025-06-05 05:57:23 -0700

    Make Cart Page responsive

--- src/components/CartProduct.jsx
+++ src/components/CartProduct.jsx
@@ -1,25 +1,64 @@
 import { faMinusSquare, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {useCart} from "../hooks/useCartProvider"
+import { useCart } from "../hooks/useCartProvider";
 const CartProduct = ({ id, name, imgsrc, price, discount, quantity }) => {
-  const {removeFromCart, increaseQty, decreaseQty} = useCart()
+  const { removeFromCart, increaseQty, decreaseQty } = useCart();
   return (
-    <div key={id} className="grid grid-cols-[1fr_2fr_1fr] gap-4 w-screen h-auto border-b-[1px] border-black items-center px-3">
+    <div
+      key={id}
+      className="grid grid-cols-[1fr_2fr_1fr] md:grid-cols-5 gap-4 w-screen h-auto border-b-[1px] border-black items-center px-3"
+    >
       <div className="w-[100px] h-[100px] border-2 border-gray-500/50 rounded-[20px] my-3 mx-5">
-        <img className="w-full h-full rounded-[20px] scale-90 object-cover" src={imgsrc} alt="img" />
+        <img
+          className="w-full h-full rounded-[20px] scale-90 object-cover"
+          src={imgsrc}
+          alt="img"
+        />
       </div>
-      <main className="flex flex-col gap-1 justify-center">
+      <span className="hidden md:block md:text-[18px] lg:text-[20px]">
+        {name}
+      </span>
+      <span className="text-[16px] md:text-[20px] md:flex items-center gap-3 hidden">
+        <FontAwesomeIcon
+          onClick={() => decreaseQty(id)}
+          className="text-red-500 text-[20px] md:text-[25px] hover:cursor-pointer"
+          icon={faMinusSquare}
+        />{" "}
+        {quantity}{" "}
+        <FontAwesomeIcon
+          onClick={() => increaseQty(id)}
+          className="text-green-500 text-[20px] md:text-[25px] hover:cursor-pointer"
+          icon={faPlusSquare}
+        />
+      </span>
+      <span className="hidden md:block text-[20px] font-medium">
+        ${(price - (price * discount) / 100).toFixed(2)}
+      </span>
+      <main className="flex flex-col gap-1 justify-center md:hidden">
         <span className="text-[16px] tracking-tighter">{name}</span>
         <span className="text-[20px] font-medium">
           ${(price - (price * discount) / 100).toFixed(2)}
         </span>
         <span className="text-[16px] flex items-center gap-3">
-          <FontAwesomeIcon onClick={() => decreaseQty(id)} className="text-red-500" icon={faMinusSquare} />{" "}
+          <FontAwesomeIcon
+            onClick={() => decreaseQty(id)}
+            className="text-red-500 text-[20px]"
+            icon={faMinusSquare}
+          />{" "}
           {quantity}{" "}
-          <FontAwesomeIcon onClick={() => increaseQty(id)} className="text-green-500" icon={faPlusSquare} />
+          <FontAwesomeIcon
+            onClick={() => increaseQty(id)}
+            className="text-green-500 text-[20px]"
+            icon={faPlusSquare}
+          />
         </span>
       </main>
-      <button onClick={() => removeFromCart(id)} className="bg-red-500 active:bg-red-500/70 px-3 py-1 text-[15px] text-white rounded border-[1px] border-black/50">Remove</button>
+      <button
+        onClick={() => removeFromCart(id)}
+        className="bg-red-500 active:bg-red-500/70 lg:w-[50%] md:w-[94px] px-3 py-1 text-[15px] text-white rounded border-[1px] border-black/50 hover:cursor-pointer"
+      >
+        Remove
+      </button>
     </div>
   );
 };
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,54 +2,99 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { CartProduct } from "../components/index";
 import { useCart } from "../hooks/useCartProvider";
 import { Link } from "react-router-dom";
-import { faArrowLeft, faLongArrowAltLeft } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowLeft,
+  faLongArrowAltLeft,
+} from "@fortawesome/free-solid-svg-icons";
 const Cart = () => {
   const { cart, clearCart } = useCart();
-  const total = cart.reduce((sum, {price, quantity}) => sum += price * quantity,0)
-  const count = cart.reduce((sum, {quantity}) => sum += quantity, 0)
+  const total = cart.reduce(
+    (sum, { price, quantity }) => (sum += price * quantity),
+    0
+  );
+  const count = cart.reduce((sum, { quantity }) => (sum += quantity), 0);
   return (
-    <div>
-      <Link to='/' className="flex items-center gap-4 px-5 text-[20px]"><FontAwesomeIcon className="text-[20px] mt-1" icon={faLongArrowAltLeft}/>Back</Link>
-      <header className="flex flex-col justify-center items-center border-b-[1px] border-black py-6">
-        <div className="flex justify-between items-center px-5 pb-3 w-full">
-          <span className="text-[30px] font-medium font-serif pb-3">My Bag</span>
-          <span className="font-medium">Total {count} Items</span>
+    <div className="flex flex-col items-center justify-center">
+      <Link
+        to="/"
+        className="flex items-center gap-4 px-5 text-[20px] md:text-[25px] w-full mt-3"
+      >
+        <FontAwesomeIcon
+          className="text-[20px] md:text-[25px] mt-1 bg-green-300 p-2 rounded"
+          icon={faLongArrowAltLeft}
+        />
+        Back
+      </Link>
+      <header className="flex flex-col justify-center items-center border-b-[1px] border-black py-6 w-full">
+        <div className="flex justify-between md:w-[80%] items-center px-5 pb-3 w-full">
+          <span className="text-[30px] md:text-[50px] font-medium font-serif pb-3 underline underline-offset-6">
+            My Cart
+          </span>
+          <span className="font-medium md:text-[20px]">
+            Total {count} Items
+          </span>
         </div>
-        {cart.length > 0 && (<button onClick={() => clearCart()} className="bg-blue-500 active:bg-blue-500/80 w-[85%] py-2 text-[15px] text-white rounded border-[1px] border-black/50">Empty Cart</button>)}
+        {cart.length > 0 && (
+          <button
+            onClick={() => clearCart()}
+            className="bg-blue-500 active:bg-blue-500/80 w-[50%] py-2 text-[15px] text-white rounded border-[1px] border-black/50 md:hidden"
+          >
+            Empty Cart
+          </button>
+        )}
+        {cart.length > 0 && (
+          <div className="hidden md:grid grid-cols-[2fr_1fr_1fr_1fr] w-full px-[50px] mt-[20px] items-center">
+            <span>Items</span>
+            <span className="ms-1">Quantity</span>
+            <span className="lg:ms-4 md:ms-5">Price</span>
+            {cart.length > 0 && (
+              <button
+                onClick={() => clearCart()}
+                className="bg-blue-500 active:bg-blue-500/80 lg:w-[50%] md:w-[65%] py-2 text-[15px] text-white rounded border-[1px] border-black/50 hidden md:block ms-[37px] hover:cursor-pointer"
+              >
+                Empty Cart
+              </button>
+            )}
+          </div>
+        )}
       </header>
-      
-      {cart.length === 0 ? (
-      <div className="text-center text-gray-600 font-medium p-10">There is no item in the cart.</div>
-    ) : (
-      <>
-        <div>
-          {cart.map(({ id, name, imgsrc, price, discount, quantity }) => (
-            <CartProduct
-              key={id}
-              id={id}
-              name={name}
-              imgsrc={imgsrc}
-              price={price}
-              discount={discount}
-              quantity={quantity}
-            />
-          ))}
-        </div>
 
-        <footer className="my-3 flex flex-col justify-center items-center">
-          <div className="flex justify-between px-5 items-center h-[50px] w-screen">
-            <span className="text-[14px] font-mono font-bold">TOTAL</span>
-            <span className="text-[20px] font-medium">${total.toFixed(2)}</span>
+      {cart.length === 0 ? (
+        <div className="text-center text-gray-600 font-medium p-10">
+          There is no item in the cart.
+        </div>
+      ) : (
+        <>
+          <div>
+            {cart.map(({ id, name, imgsrc, price, discount, quantity }) => (
+              <CartProduct
+                key={id}
+                id={id}
+                name={name}
+                imgsrc={imgsrc}
+                price={price}
+                discount={discount}
+                quantity={quantity}
+              />
+            ))}
           </div>
-          <div className="flex justify-center items-center px-7 w-screen">
-            <Link className="text-[20px] text-white text-center bg-pink-600/80 active:bg-pink-600/60 w-full py-2 rounded-[5px]">
-              Next
-            </Link>
-          </div>
-        </footer>
-      </>
-    )}
-      
+          <footer className="my-3 flex flex-col justify-center items-center">
+            <div className="flex justify-between md:justify-end md:gap-[100px] md:pe-[70px] lg:gap-[230px] lg:pe-[170px] px-5 items-center h-[50px] w-screen">
+              <span className="text-[14px] md:text-[19px] md:mt-[2px] lg:text-[23px] font-mono font-bold md:font-normal">
+                TOTAL:
+              </span>
+              <span className="text-[20px] font-medium">
+                ${total.toFixed(2)}
+              </span>
+            </div>
+            <div className="flex justify-end items-center px-7 w-screen md:pe-[60px] lg:pe-[141px]">
+              <Link className="text-[20px] text-white text-center md:w-[255px] lg:w-fit lg:px-5 bg-green-600/90 active:bg-green-600/60 w-screen py-2 rounded-[5px] hover:cursor-pointer">
+                Check Out
+              </Link>
+            </div>
+          </footer>
+        </>
+      )}
     </div>
   );
 };
